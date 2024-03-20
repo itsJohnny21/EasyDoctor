a9da6fb88c3416551dd07157dc7fbf1aea3fa142
itsJohnny21 <jsalazar6421@gmail.com>
2024-03-19 23:40:43 -0700

    Working eclipse version

---
+++ src/application/controllers/Controller.java
@@ -0,0 +1,26 @@
+package application.controllers;
+
+import javafx.stage.Stage;
+
+public abstract class Controller {
+
+	public String title;
+	public Stage stage;
+
+	public double windowMaxWidth;
+	public double windowMaxHeight;
+
+	public double windowMinWidth;
+	public double windowMinHeight;
+
+	public double preferredWindowWidth;
+	public double preferredWindowHeight;
+
+	public boolean resizeable;
+
+	public void setStage(Stage stage) {
+		this.stage = stage;
+	}
+
+	public abstract String getTitle();
+}
