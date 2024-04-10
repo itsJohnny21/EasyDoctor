706912873a141a4b5081e68da1892cd2151db656
itsJohnny21 <jsalazar6421@gmail.com>
2024-04-10 14:04:33 -0700

    If the Database.role variable is null, then it means the database needs to connect as 'neutral'

--- src/main/java/edu/asu/easydoctor/Database.java
+++ src/main/java/edu/asu/easydoctor/Database.java
@@ -111,6 +111,8 @@ public abstract class Database {
     }
 
     public static String getGrantee() {
+        if (role == null) return "\"'neutral'@'%'\"";
+        
         return String.format("\"'%s'@'%%'\"", role.toString().toLowerCase());
     }
 
@@ -153,6 +155,7 @@ public abstract class Database {
         String colummns = getPermissedColumns(tableName, "SELECT");
         PreparedStatement statement2 = connection.prepareStatement(String.format("SELECT %s FROM %s WHERE ID = ?;", colummns, tableName));
         statement2.setInt(1, rowID);
+        System.out.println(statement2.toString());
 
         ResultSet resultSet2 = statement2.executeQuery();
 
@@ -382,10 +385,23 @@ public abstract class Database {
     }
 
     public static String getMy(String columnName) throws Exception {
+        PreparedStatement statement;
         if (role == Role.PATIENT) {
-            return selectRow(0, "patients").getString(columnName);
+            statement = connection.prepareStatement(String.format("SELECT %s FROM patients WHERE ID = ?;", columnName));
+            statement.setInt(1, userID);
+
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
+
+            return resultSet.getString(columnName);
         } else {
-            return selectRow(0, "employees").getString(columnName);
+            statement = connection.prepareStatement(String.format("SELECT %s FROM employees WHERE ID = ?;", columnName));
+            statement.setInt(1, userID);
+
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
+
+            return resultSet.getString(columnName);
         }
     }
 
