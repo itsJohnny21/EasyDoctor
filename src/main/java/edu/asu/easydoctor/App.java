b89dc64a8a204d97eb357d90e00846cdef1f8cf0
itsJohnny21 <jsalazar6421@gmail.com>
2024-04-10 13:08:18 -0700

    Commit by Johnny Salazar

--- src/main/java/edu/asu/easydoctor/App.java
+++ src/main/java/edu/asu/easydoctor/App.java
@@ -37,6 +37,7 @@ public class App extends Application {
 		System.exit(0);
 	}
 	public static void main(String[] args) {
+		System.out.println("This is for my commit for Phase III documentation");
 		System.out.println("Starting application...");
 		try {
 			launch(args);
