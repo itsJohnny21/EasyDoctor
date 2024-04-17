3043dd0db66d8944efa46ea690f99716dc6ac42f
143mailliw <wwokwilliam@gmail.com>
2024-04-16 22:12:40 -0700

    test: setup for junit

---
+++ src/test/java/edu/asu/easydoctor/Tests.java
@@ -0,0 +1,14 @@
+package edu.asu.easydoctor;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Tests {
+    @Test   
+    @DisplayName("scheduleVisit")
+    public void scheduleVisit() throws Exception {
+        assertEquals(1, 1);
+    }
+}
---
+++ src/test/java/module-info.java
@@ -0,0 +1,5 @@
+module edu.asu.easydoctor {
+    requires org.junit.jupiter.api;
+
+    exports edu.asu.easydoctor to org.junit.platform.commons;
+}
