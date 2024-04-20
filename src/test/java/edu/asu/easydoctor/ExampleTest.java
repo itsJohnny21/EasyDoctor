0ddd0dd879fb0db973d84cd2889654b83b8346bd
itsJohnny21 <jsalazar6421@gmail.com>
2024-04-19 18:42:27 -0700

    A basic template for testing methods with JUnit

---
+++ src/test/java/edu/asu/easydoctor/ExampleTest.java
@@ -0,0 +1,14 @@
+package edu.asu.easydoctor;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+    @Test
+    @DisplayName("exampleTest")
+    public void exampleTest() {
+        assertEquals(1, 1);
+    }
+}
